63b2bee42c1fa89e9aa06b2c9b37e585d4d4cecf
Daniel Waltz <danielbwaltz@gmail.com>
2023-06-25 08:59:25 -0400

    feat: initial draft of the home page

--- app.vue
+++ app.vue
@@ -1,5 +1,81 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div
+    class="bg-black text-white h-full min-h-100vh grid grid-cols-[1fr_1fr] font-sans"
+  >
+    <div class="text-black bg-primary flex flex-col gap-8 p-10">
+      <header>
+        <h1
+          class="flex items-center uppercase font-bold text-2xl tracking-widest lh-1em gap-5"
+        >
+          <i class="i-mdi-dots-grid scale-125" />
+          Natalie Fischesser
+        </h1>
+      </header>
+
+      <main class="flex items-center grow">
+        <span class="uppercase font-bold lh-1em text-32">
+          Hey! What's good?
+        </span>
+      </main>
+    </div>
+
+    <aside
+      class="grid grid-cols-[1fr_1fr_1fr_1fr_1fr] grid-rows-[1fr_1fr_1fr_1fr_1fr_1fr_1fr] text-center gap-8 p-10 items-start"
+    >
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-yellow"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-blue"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-red col-span-3"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-pink col-span-4"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-yellow"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-yellow col-span-2"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-blue"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-red col-span-2"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-blue"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-green col-span-3"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-yellow"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-pink col-span-full"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-red"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-blue col-span-2"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-pink"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-green"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-yellow col-span-4"
+      />
+      <div
+        class="flex items-center justify-center rounded-full w-full h-full bg-red"
+      />
+    </aside>
   </div>
 </template>
