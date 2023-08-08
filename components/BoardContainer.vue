13117cf08097ca6f7a84e783976bdafb9e05bd86
Daniel Waltz <danielbwaltz@gmail.com>
2023-08-07 21:42:44 -0400

    feat: add graphics to home page

--- components/BoardContainer.vue
+++ components/BoardContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="grid grid-cols-5 grid-rows-7 gap-8 text-center text-0.6vi xl:text-0.3vi"
+    class="grid grid-cols-5 grid-rows-7 w-130em gap-8 text-center text-0.6vi xl:text-0.3vi"
   >
     <slot />
   </div>
