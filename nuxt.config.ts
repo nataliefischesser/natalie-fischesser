eeb7bc73356131d550a5eccc04977907c6e44670
Daniel Waltz <danielbwaltz@gmail.com>
2024-07-04 21:13:20 -0400

    chore: add nuxt compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ import {
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-07-04',
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
