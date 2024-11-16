b74f222f3f4e130cc8c893f49d1d3375a14c07d1
Daniel Waltz <danielbwaltz@gmail.com>
2024-11-16 10:58:12 -0500

    feat: update favicon definition

--- app/components/AppLayout.vue
+++ app/components/AppLayout.vue
@@ -11,8 +11,8 @@ useHead({
   titleTemplate: (title) =>
     title ? `${title} - Natalie Fischesser` : meta.title,
   link: [
-    { rel: "icon", href: "/favicon.ico", sizes: "any" },
-    { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
+    { rel: "icon", href: "/favicon.ico", sizes: "48x48" },
+    { rel: "icon", href: "/favicon.svg", sizes: "any", type: "image/svg+xml" },
   ],
   htmlAttrs: { lang: "en" },
   bodyAttrs: {
