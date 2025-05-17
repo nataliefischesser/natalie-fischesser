cb178b94abe0c06f18815b4320212cdcf45c829d
Daniel Waltz <danielbwaltz@gmail.com>
2025-05-16 22:06:39 -0400

    chore: enable typed lint rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,6 +43,11 @@ export default defineNuxtConfig({
       nuxt: {
         sortConfigKeys: true,
       },
+      typescript: {
+        tsconfigPath: "./tsconfig.json",
+      },
+    },
+  },
   fonts: {
     defaults: {
       weights: [400, 600, 700, 900],
