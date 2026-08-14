2e45ec4fcbaeae169fb4778463bdebe807839af9
Daniel Waltz <danielbwaltz@gmail.com>
2026-08-14 12:03:27 -0400

    chore: update vue compiler options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -71,9 +71,13 @@ export default defineNuxtConfig({
         erasableSyntaxOnly: true,
       },
       vueCompilerOptions: {
-        strictTemplates: true,
-        fallthroughAttributes: true,
         checkRequiredFallthroughAttributes: true,
+        fallthroughAttributes: true,
+        inferTemplateDollarAttrs: true,
+        inferTemplateDollarEl: true,
+        inferTemplateDollarRefs: true,
+        inferTemplateDollarSlots: true,
+        strictTemplates: true,
       },
     },
     sharedTsConfig: {
